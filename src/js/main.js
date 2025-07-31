b716b1d10f2b8e87a6ae7ef35cad6711ae95df70
Swetony <swetonyancelmo@gmail.com>
2025-07-31 19:25:04 -0300

    finalizacao do header e mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,19 @@
 const btn = document.querySelector('#menu-btn');
 const nav = document.querySelector('#nav');
+const buttonIcon = document.querySelector('#button-icon');
+
+let menuAberto = false;
 
 btn.addEventListener('click', () => {
+  menuAberto = !menuAberto;
   nav.classList.toggle('active');
-})
+
+  if(menuAberto){
+    buttonIcon.classList.remove('fa-bars');
+    buttonIcon.classList.add('fa-xmark');
+  } else {
+    buttonIcon.classList.remove('fa-xmark');
+    buttonIcon.classList.add('fa-bars');
+  }
+
+});
